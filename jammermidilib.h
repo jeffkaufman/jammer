482b21cd07162f6252ecfaca62ab0c464c0b6a92
Jeff Kaufman <jeff@jefftk.com>
2018-06-05 20:39:07 -0400

    support selfless mode

--- jammermidilib.h
+++ jammermidilib.h
@@ -21,6 +21,7 @@ void jml_attempt(OSStatus result, char* errmsg) {
 MIDIClientRef jml_midiclient;
 MIDIEndpointRef jml_midiendpoint;
 MIDIPortRef jml_midiport;
+bool newModel;
 
 void jml_compose_midi(char actionType, int noteNo, int v, Byte* msg) {
   msg[0] = actionType + ((JML_CHANNEL-1) & 0xFF);
@@ -72,70 +73,178 @@ MIDIEndpointRef getEndpointRef() {
 
     printf("Saw %s\n", CFStringGetCStringPtr(name, kCFStringEncodingUTF8));
 
-    if (CFStringCompare(name, CFSTR("AXIS-49 USB Keyboard"), 0) !=
-        kCFCompareEqualTo) continue;
-
-    return src;
+    if (CFStringCompare(name, CFSTR("AXIS-49 USB Keyboard"), 0) ==
+        kCFCompareEqualTo) {
+      newModel = false;
+      return src;
+    } else if (CFStringCompare(name, CFSTR("AXIS-49 2A"), 0) ==
+               kCFCompareEqualTo) {
+      newModel = true;
+      return src;
+    }
   }
   jml_die("Failed to locate an AXIS-49");
   return 0;  // not reached
 }
 
 char mapping(unsigned char note_in) {
-  //TODO: map the rest of the notes
-  //TODO: figure out how to get it into selfless mode
-  switch (note_in) {
-  case 25: return 37;  // Db / C#
-  case 32: return 39;  // D# / Eb
-  case 39: return 41;  // F
-  case 46: return 43;  // G
-  case 28: return 44;  // G# / Ab
-  case 53: return 45;  // A
-  case 35: return 46;  // Bb
-  case 60: return 47;  // B
-  case 42: return 48;  // C
-  case 24: return 49;  // C# / Db
-  case 67: return 49;  // C# / Db
-  case 49: return 50;  // D
-  case 31: return 51;  // D# / Eb
-  case 56: return 52;  // E
-  case 38: return 53;  // F
-  case 63: return 54;  // F#
-  case 45: return 55;  // G
-  case 70: return 56;  // G# / Ab
-  case 27: return 56;  // G# / Ab
-  case 52: return 57;  // A
-  case 34: return 58;  // Bb
-  case 59: return 59;  // B
-  case 41: return 60;  // C
-  case 66: return 61;  // C# / Db
-  case 48: return 62;  // D
-  case 30: return 63;  // D# / Eb
-  case 55: return 64;  // E
-  case 37: return 65;  // F
-  case 62: return 66;  // F#
-  case 44: return 67;  // G
-  case 69: return 68;  // G# / Ab
-  case 26: return 68;  // G# / Ab
-  case 51: return 69;  // A
-  case 33: return 70;  // Bb
-  case 58: return 71;  // B
-  case 40: return 72;  // C
-  case 65: return 73;  // C# / Db
-  case 47: return 74;  // D
-  case 72: return 75;  // D# / Eb
-  case 29: return 75;  // D# / Eb
-  case 54: return 76;  // E
-  case 36: return 77;  // F
-  case 61: return 78;  // F#
-  case 43: return 79;  // G
-  case 68: return 80;  // G# / Ab
-  case 50: return 81;  // A
-  case 57: return 83;  // B
-  case 64: return 85;  // C# / Db
-  case 71: return 87;  // D# / Eb
-  default:
-    return 0;
+  if (newModel) {
+    switch(note_in) {
+    case 1: return 123;  // D#
+    case 2: return 121;  // C#
+    case 3: return 119;  // B
+    case 4: return 117;  // A
+    case 5: return 115;  // G
+    case 6: return 113;  // F
+    case 7: return 111;  // Eb
+    case 8: return 116;  // G#
+    case 9: return 114;  // F#
+    case 10: return 112;  // E
+    case 11: return 110;  // D
+    case 12: return 108;  // C
+    case 13: return 106;  // Bb
+    case 14: return 104;  // Ab
+    case 15: return 111;  // D#
+    case 16: return 109;  // C#
+    case 17: return 107;  // B
+    case 18: return 105;  // A
+    case 19: return 103;  // G
+    case 20: return 101;  // F
+    case 21: return 99;  // Eb
+    case 22: return 104;  // G#
+    case 23: return 102;  // F#
+    case 24: return 100;  // E
+    case 25: return 98;  // D
+    case 26: return 96;  // C
+    case 27: return 94;  // Bb
+    case 28: return 92;  // Ab
+    case 29: return 99;  // D#
+    case 30: return 97;  // C#
+    case 31: return 95;  // B
+    case 32: return 93;  // A
+    case 33: return 91;  // G
+    case 34: return 89;  // F
+    case 35: return 87;  // Eb
+    case 36: return 92;  // G#
+    case 37: return 90;  // F#
+    case 38: return 88;  // E
+    case 39: return 86;  // D
+    case 40: return 84;  // C
+    case 41: return 82;  // Bb
+    case 42: return 80;  // Ab
+    case 43: return 87;  // D#
+    case 44: return 85;  // C#
+    case 45: return 83;  // B
+    case 46: return 81;  // A
+    case 47: return 79;  // G
+    case 48: return 77;  // F
+    case 49: return 75;  // Eb
+    case 50: return 80;  // G#
+    case 51: return 78;  // F#
+    case 52: return 76;  // E
+    case 53: return 74;  // D
+    case 54: return 72;  // C
+    case 55: return 70;  // Bb
+    case 56: return 68;  // Ab
+    case 57: return 73;  // C#
+    case 58: return 71;  // B
+    case 59: return 69;  // A
+    case 60: return 67;  // G
+    case 61: return 65;  // F
+    case 62: return 63;  // Eb
+    case 63: return 61;  // Db
+    case 64: return 68;  // G#
+    case 65: return 66;  // F#
+    case 66: return 64;  // E
+    case 67: return 62;  // D
+    case 68: return 60;  // C
+    case 69: return 58;  // Bb
+    case 70: return 56;  // Ab
+    case 71: return 61;  // C#
+    case 72: return 59;  // B
+    case 73: return 57;  // A
+    case 74: return 55;  // G
+    case 75: return 53;  // F
+    case 76: return 51;  // Eb
+    case 77: return 49;  // Db
+    case 78: return 56;  // G#
+    case 79: return 54;  // F#
+    case 80: return 52;  // E
+    case 81: return 50;  // D
+    case 82: return 48;  // C
+    case 83: return 46;  // Bb
+    case 84: return 44;  // Ab
+    case 85: return 49;  // C#
+    case 86: return 47;  // B
+    case 87: return 45;  // A
+    case 88: return 43;  // G
+    case 89: return 41;  // F
+    case 90: return 39;  // Eb
+    case 91: return 37;  // Db
+    case 92: return 44;  // G#
+    case 93: return 42;  // F#
+    case 94: return 40;  // E
+    case 95: return 38;  // D
+    case 96: return 36;  // C
+    case 97: return 34;  // Bb
+    case 98: return 32;  // Ab
+    default:
+      return 0;
+    }
+  } else {
+    switch (note_in) {
+    case 25: return 37;  // Db / C#
+    case 32: return 39;  // D# / Eb
+    case 39: return 41;  // F
+    case 46: return 43;  // G
+    case 28: return 44;  // G# / Ab
+    case 53: return 45;  // A
+    case 35: return 46;  // Bb
+    case 60: return 47;  // B
+    case 42: return 48;  // C
+    case 24: return 49;  // C# / Db
+    case 67: return 49;  // C# / Db
+    case 49: return 50;  // D
+    case 31: return 51;  // D# / Eb
+    case 56: return 52;  // E
+    case 38: return 53;  // F
+    case 63: return 54;  // F#
+    case 45: return 55;  // G
+    case 70: return 56;  // G# / Ab
+    case 27: return 56;  // G# / Ab
+    case 52: return 57;  // A
+    case 34: return 58;  // Bb
+    case 59: return 59;  // B
+    case 41: return 60;  // C
+    case 66: return 61;  // C# / Db
+    case 48: return 62;  // D
+    case 30: return 63;  // D# / Eb
+    case 55: return 64;  // E
+    case 37: return 65;  // F
+    case 62: return 66;  // F#
+    case 44: return 67;  // G
+    case 69: return 68;  // G# / Ab
+    case 26: return 68;  // G# / Ab
+    case 51: return 69;  // A
+    case 33: return 70;  // Bb
+    case 58: return 71;  // B
+    case 40: return 72;  // C
+    case 65: return 73;  // C# / Db
+    case 47: return 74;  // D
+    case 72: return 75;  // D# / Eb
+    case 29: return 75;  // D# / Eb
+    case 54: return 76;  // E
+    case 36: return 77;  // F
+    case 61: return 78;  // F#
+    case 43: return 79;  // G
+    case 68: return 80;  // G# / Ab
+    case 50: return 81;  // A
+    case 57: return 83;  // B
+    case 64: return 85;  // C# / Db
+    case 71: return 87;  // D# / Eb
+    default:
+      return 0;
+    }
   }
 }
 
@@ -151,7 +260,8 @@ void read_midi(const MIDIPacketList *pktlist,
       unsigned char note_in = packet->data[1];
       unsigned char val = packet->data[2];
 
-      unsigned char note_out = mapping(note_in) + 12;
+      unsigned char note_out = mapping(note_in)
+        + (newModel ? -24 : +12);
 
       jml_send_midi(mode, note_out, val);
 
