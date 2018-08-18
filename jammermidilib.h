4968a058a9e4c7ce2cb1a911e638eed1bc5a1bbd
Jeff Kaufman <jeff@jefftk.com>
2018-08-18 15:08:00 -0400

    polyphony v2

--- jammermidilib.h
+++ jammermidilib.h
@@ -45,6 +45,8 @@ bool new_model;
 
 int pipes_note = -1;
 
+bool polyphonic = false;
+
 char mapping(unsigned char note_in) {
   if (new_model) {
     switch(note_in) {
@@ -206,13 +208,141 @@ char mapping(unsigned char note_in) {
   }
 }
 
+unsigned char polyphonic_mapping(unsigned char note_in) {
+  switch(note_in) {
+
+  // instrument 0
+  case 1: return 1;    // Db
+  case 2: return 3;    // Eb
+  case 3: return 5;    // F
+  case 4: return 7;    // G
+  case 5: return 9;    // A
+  case 6: return 11;   // B
+  case 7: return 13;   // C#
+
+  case 8: return 8;    // Ab
+  case 9: return 10;   // Bb
+  case 10: return 12;  // C
+  case 11: return 14;  // D
+  case 12: return 16;  // E
+  case 13: return 18;  // F#
+  case 14: return 20;  // G#
+  case 15: return 13;  // Db
+  case 16: return 15;  // Eb
+  case 17: return 17;  // F
+  case 18: return 19;  // G
+  case 19: return 21;  // A
+  case 20: return 23;  // B
+  case 21: return 25;  // C#
+
+  // instrument 1
+  case 22: return 8;  // Ab
+  case 23: return 10;  // Bb
+  case 24: return 12;  // C
+  case 25: return 14;  // D
+  case 26: return 16;  // E
+  case 27: return 18;  // F#
+  case 28: return 20;  // G#
+  case 29: return 13;  // Db
+  case 30: return 15;  // Eb
+  case 31: return 17;  // F
+  case 32: return 19;  // G
+  case 33: return 21;  // A
+  case 34: return 23;  // B
+  case 35: return 25;  // C#
+
+  case 36: return 20;  // Ab
+  case 37: return 22;  // Bb
+  case 38: return 24;  // C
+  case 39: return 26;  // D
+  case 40: return 28;  // E
+  case 41: return 30;  // F#
+  case 42: return 32;  // G#
+  case 43: return 25;  // Db
+  case 44: return 27;  // Eb
+  case 45: return 29;  // F
+  case 46: return 31;  // G
+  case 47: return 33;  // A
+  case 48: return 35;  // B
+  case 49: return 37;  // C#
+
+  // instrument 0  
+  case 50: return 20;  // Ab
+  case 51: return 22;  // Bb
+  case 52: return 24;  // C
+  case 53: return 26;  // D
+  case 54: return 28;  // E
+  case 55: return 30;  // F#
+  case 56: return 32;  // G#
+  case 57: return 27;  // Eb
+  case 58: return 29;  // F
+  case 59: return 31;  // G
+  case 60: return 33;  // A
+  case 61: return 35;  // B
+  case 62: return 37;  // C#
+  case 63: return 39;  // D#
+
+  case 64: return 32;  // Ab
+  case 65: return 34;  // Bb
+  case 66: return 36;  // C
+  case 67: return 38;  // D
+  case 68: return 40;  // E
+  case 69: return 42;  // F#
+  case 70: return 44;  // G#
+  case 71: return 39;  // Eb
+  case 72: return 41;  // F
+  case 73: return 43;  // G
+  case 74: return 45;  // A
+  case 75: return 47;  // B
+  case 76: return 49;  // C#
+  case 77: return 51;  // D#
+
+  // instrument 1
+  case 78: return 32;  // Ab
+  case 79: return 34;  // Bb
+  case 80: return 36;  // C
+  case 81: return 38;  // D
+  case 82: return 40;  // E
+  case 83: return 42;  // F#
+  case 84: return 44;  // G#
+  case 85: return 39;  // Eb
+  case 86: return 41;  // F
+  case 87: return 43;  // G
+  case 88: return 45;  // A
+  case 89: return 47;  // B
+  case 90: return 49;  // C#
+  case 91: return 51;  // D#
+
+  case 92: return 44;  // Ab
+  case 93: return 46;  // Bb
+  case 94: return 48;  // C
+  case 95: return 50;  // D
+  case 96: return 52;  // E
+  case 97: return 54;  // F#
+  case 98: return 56;  // G
+  default: return 0;
+  }
+}
+
+int polyphonic_instrument(unsigned char note_in) {
+  if (note_in < 22) {
+    return 0;
+  } else if (note_in < 50) {
+    return 1;
+  } else if (note_in < 78) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
 #define PACKET_BUF_SIZE (3+64) /* 3 for message, 32 for structure vars */
 void send_midi(char actionType, int noteNo, int v, int endpoint) {
-  printf("sending %d %x:%d = %d\n",
-         endpoint,
-         (unsigned char) actionType,
-         noteNo,
-         v);
+  //printf("sending %d %x:%d = %d\n",
+  //       endpoint,
+  //       (unsigned char) actionType,
+  //       noteNo,
+  //      v);
 
   Byte buffer[PACKET_BUF_SIZE];
   Byte msg[3];
@@ -280,7 +410,7 @@ void read_midi(const MIDIPacketList *pktlist,
       unsigned int note_in = packet->data[1];
       unsigned int val = packet->data[2];
 
-      printf("got packet %x %x %x\n", mode, note_in, val);
+      //printf("got packet %x %d %x\n", mode, note_in, val);
 
       if (mode == MIDI_ON && val == 0) {
         mode = MIDI_OFF;
@@ -291,17 +421,28 @@ void read_midi(const MIDIPacketList *pktlist,
 
         if (note_in == 1 || note_in == 2) {
           // switch polyphonic mode
-          printf("polyphonic not implemented\n");
-          all_notes_off();
+          polyphonic = (note_in == 2);
+          all_notes_off();          
         } else if (note_in >= 3 && note_in < 3 + N_ENDPOINTS) {
           // switch default endpoint for switching instruments
           current_instrument = note_in - 3;
           printf("switched to endpoint %d\n", current_instrument);
           all_notes_off();
+        } else if (polyphonic) {
+          int instrument = polyphonic_instrument(note_in);
+          unsigned char note_out = polyphonic_mapping(note_in) + 2 + 12 + 12;
+
+          printf("%d -> %d / %d\n",
+                 note_in, instrument, note_out);
+
+          int endpoint = instrument ? TROMBONE : SAX;
+          if (endpoint == SAX) {
+            note_out += 12;
+          }
+          send_midi(mode, note_out, val, endpoint);
         } else {
           unsigned char note_out = mapping(note_in) + 12 + 2;
           int endpoint = current_instrument;
-
           if (endpoint == PIPES) {
             val = 127;
             if (mode == MIDI_ON) {
