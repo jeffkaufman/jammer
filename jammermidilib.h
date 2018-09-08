8645cf5f0489015d213bb2ce9b1c7df10265063d
Jeff Kaufman <jeff@jefftk.com>
2018-09-08 07:14:15 -0400

    remove polyphonic mode; add radio button mode

--- jammermidilib.h
+++ jammermidilib.h
@@ -43,9 +43,9 @@ int current_instrument;
 
 bool new_model;
 
-int pipes_note = -1;
+int current_note[N_ENDPOINTS];
 
-bool polyphonic = false;
+bool radio_buttons = false;
 
 char mapping(unsigned char note_in) {
   if (new_model) {
@@ -208,134 +208,6 @@ char mapping(unsigned char note_in) {
   }
 }
 
-unsigned char polyphonic_mapping(unsigned char note_in) {
-  switch(note_in) {
-
-  // instrument 0
-  case 1: return 1;    // Db
-  case 2: return 3;    // Eb
-  case 3: return 5;    // F
-  case 4: return 7;    // G
-  case 5: return 9;    // A
-  case 6: return 11;   // B
-  case 7: return 13;   // C#
-
-  case 8: return 8;    // Ab
-  case 9: return 10;   // Bb
-  case 10: return 12;  // C
-  case 11: return 14;  // D
-  case 12: return 16;  // E
-  case 13: return 18;  // F#
-  case 14: return 20;  // G#
-  case 15: return 13;  // Db
-  case 16: return 15;  // Eb
-  case 17: return 17;  // F
-  case 18: return 19;  // G
-  case 19: return 21;  // A
-  case 20: return 23;  // B
-  case 21: return 25;  // C#
-
-  // instrument 1
-  case 22: return 8;  // Ab
-  case 23: return 10;  // Bb
-  case 24: return 12;  // C
-  case 25: return 14;  // D
-  case 26: return 16;  // E
-  case 27: return 18;  // F#
-  case 28: return 20;  // G#
-  case 29: return 13;  // Db
-  case 30: return 15;  // Eb
-  case 31: return 17;  // F
-  case 32: return 19;  // G
-  case 33: return 21;  // A
-  case 34: return 23;  // B
-  case 35: return 25;  // C#
-
-  case 36: return 20;  // Ab
-  case 37: return 22;  // Bb
-  case 38: return 24;  // C
-  case 39: return 26;  // D
-  case 40: return 28;  // E
-  case 41: return 30;  // F#
-  case 42: return 32;  // G#
-  case 43: return 25;  // Db
-  case 44: return 27;  // Eb
-  case 45: return 29;  // F
-  case 46: return 31;  // G
-  case 47: return 33;  // A
-  case 48: return 35;  // B
-  case 49: return 37;  // C#
-
-  // instrument 0  
-  case 50: return 20;  // Ab
-  case 51: return 22;  // Bb
-  case 52: return 24;  // C
-  case 53: return 26;  // D
-  case 54: return 28;  // E
-  case 55: return 30;  // F#
-  case 56: return 32;  // G#
-  case 57: return 27;  // Eb
-  case 58: return 29;  // F
-  case 59: return 31;  // G
-  case 60: return 33;  // A
-  case 61: return 35;  // B
-  case 62: return 37;  // C#
-  case 63: return 39;  // D#
-
-  case 64: return 32;  // Ab
-  case 65: return 34;  // Bb
-  case 66: return 36;  // C
-  case 67: return 38;  // D
-  case 68: return 40;  // E
-  case 69: return 42;  // F#
-  case 70: return 44;  // G#
-  case 71: return 39;  // Eb
-  case 72: return 41;  // F
-  case 73: return 43;  // G
-  case 74: return 45;  // A
-  case 75: return 47;  // B
-  case 76: return 49;  // C#
-  case 77: return 51;  // D#
-
-  // instrument 1
-  case 78: return 32;  // Ab
-  case 79: return 34;  // Bb
-  case 80: return 36;  // C
-  case 81: return 38;  // D
-  case 82: return 40;  // E
-  case 83: return 42;  // F#
-  case 84: return 44;  // G#
-  case 85: return 39;  // Eb
-  case 86: return 41;  // F
-  case 87: return 43;  // G
-  case 88: return 45;  // A
-  case 89: return 47;  // B
-  case 90: return 49;  // C#
-  case 91: return 51;  // D#
-
-  case 92: return 44;  // Ab
-  case 93: return 46;  // Bb
-  case 94: return 48;  // C
-  case 95: return 50;  // D
-  case 96: return 52;  // E
-  case 97: return 54;  // F#
-  case 98: return 56;  // G
-  default: return 0;
-  }
-}
-
-int polyphonic_instrument(unsigned char note_in) {
-  if (note_in < 22) {
-    return 0;
-  } else if (note_in < 50) {
-    return 1;
-  } else if (note_in < 78) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
 #define PACKET_BUF_SIZE (3+64) /* 3 for message, 32 for structure vars */
 void send_midi(char actionType, int noteNo, int v, int endpoint) {
   //printf("sending %d %x:%d = %d\n",
@@ -420,44 +292,32 @@ void read_midi(const MIDIPacketList *pktlist,
         // note on or off
 
         if (note_in == 1 || note_in == 2) {
-          // switch polyphonic mode
-          polyphonic = (note_in == 2);
+          radio_buttons = (note_in == 2);
           all_notes_off();          
         } else if (note_in >= 3 && note_in < 3 + N_ENDPOINTS) {
           // switch default endpoint for switching instruments
           current_instrument = note_in - 3;
           printf("switched to endpoint %d\n", current_instrument);
           all_notes_off();
-        } else if (polyphonic) {
-          int instrument = polyphonic_instrument(note_in);
-          unsigned char note_out = polyphonic_mapping(note_in) + 2 + 12 + 12;
-
-          printf("%d -> %d / %d\n",
-                 note_in, instrument, note_out);
-
-          int endpoint = instrument ? TROMBONE : SAX;
-          if (endpoint == SAX) {
-            note_out += 12;
-          }
-          send_midi(mode, note_out, val, endpoint);
         } else {
           unsigned char note_out = mapping(note_in) + 12 + 2;
           int endpoint = current_instrument;
-          if (endpoint == PIPES) {
+
+          if (endpoint == PIPES || endpoint == ACCORDION) {
             val = 127;
+          }
+
+          if (radio_buttons) {
             if (mode == MIDI_ON) {
-              if (pipes_note != -1) {
-                send_midi(MIDI_OFF, pipes_note, 0, endpoint);
+              if (current_note[endpoint] != -1) {
+                send_midi(MIDI_OFF, current_note[endpoint], 0, endpoint);
               }
-              pipes_note = note_out;
+              current_note[endpoint] = note_out;
               send_midi(MIDI_ON, note_out, val, endpoint);
             } else if (mode == MIDI_OFF) {
               // ignore
             }
           } else {
-            if (endpoint == ACCORDION) {
-              val = 127;
-            }
             send_midi(mode, note_out, val, endpoint);
           }
         }
@@ -467,19 +327,20 @@ void read_midi(const MIDIPacketList *pktlist,
           if (note_in == 0x02) { // breath controller
             note_in = 0x0b; // send CC-11 instead of CC-2
           }
-          if (endpoint == ACCORDION) {
-            // accordion always should have some volume
+
+          if (endpoint == ACCORDION && !radio_buttons) {
+            // accordion always should have some volume in polyphonic mode
             val = val + 18;
             if (val > 127) {
               val = 127;
             }
           }
-          if (endpoint == PIPES) {
-            if (val == 0 && pipes_note != -1) {
-              send_midi(MIDI_OFF, pipes_note, 0, endpoint);
-              pipes_note = -1;
-            }
-          } else if (endpoint == KEYBOARD) {
+          if (radio_buttons && endpoint == PIPES && val == 0 && current_note[endpoint] != -1) {
+            send_midi(MIDI_OFF, current_note[endpoint], 0, endpoint);
+            current_note[endpoint] = -1;
+          }
+
+          if (endpoint == KEYBOARD || endpoint == PIPES) {
             // skip breath
           } else {
             send_midi(MIDI_CC, note_in, val, endpoint);
@@ -557,7 +418,10 @@ void jml_setup() {
   create_source(&endpoints[PIPES], CFSTR("jammer-pipes"));
   create_source(&endpoints[KEYBOARD], CFSTR("jammer-keyboard"));
   create_source(&endpoints[ACCORDION], CFSTR("jammer-accordion"));
-
+  
+  for (int i = 0; i < N_ENDPOINTS; i++) {
+    current_note[i] = -1;
+  }
 }
 
 #endif
