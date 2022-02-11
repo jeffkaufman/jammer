0e5b6c11161ef3afd25091c4d9352193abed54dd
Jeff Kaufman <jeff@jefftk.com>
2022-02-11 19:17:37 +0000

    try turning off chorus

--- run-fluidsynth.sh
+++ run-fluidsynth.sh
@@ -10,7 +10,7 @@ if [[ "$CARD_LINE" =~ \
       ^card[[:space:]]([0-9]*):.*device[[:space:]]([0-9]*):.*$ ]]; then
     CARD=${BASH_REMATCH[1]}
     DEVICE=${BASH_REMATCH[2]}
-    fluidsynth -g 1.0 -i -L 2 -c=2 -z=8 --server --audio-driver=alsa \
+    fluidsynth -g 1.0 -i -L 2 -C no -c=2 -z=8 --server --audio-driver=alsa \
          -o audio.alsa.device=hw:"${CARD},${DEVICE}" \
          /usr/share/sounds/sf2/FluidR3_GM.sf2
 else
