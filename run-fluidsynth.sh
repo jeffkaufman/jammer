0bd38b32b17233cb2805bf0af834d1f98943dc92
Jeff Kaufman <jeff@jefftk.com>
2022-03-01 03:13:08 +0000

    run fluidsynth from source

--- run-fluidsynth.sh
+++ run-fluidsynth.sh
@@ -10,7 +10,8 @@ if [[ "$CARD_LINE" =~ \
       ^card[[:space:]]([0-9]*):.*device[[:space:]]([0-9]*):.*$ ]]; then
     CARD=${BASH_REMATCH[1]}
     DEVICE=${BASH_REMATCH[2]}
-    fluidsynth -g 1.0 -i -C no -c=2 -z=8 --server --audio-driver=alsa \
+    /home/pi/fluidsynth/build/src/fluidsynth -g 1.0 -i -C no --server \
+	 --audio-driver=alsa \
          -o audio.alsa.device=hw:"${CARD},${DEVICE}" \
          /usr/share/sounds/sf2/FluidR3_GM.sf2
 else
